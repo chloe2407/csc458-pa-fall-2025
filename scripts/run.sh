45510ae464ead9e399b3b320f7dcd19096966266
Chloe <chloelam2407@gmail.com>
2025-11-25 04:39:41 +0000

    change

--- scripts/run.sh
+++ scripts/run.sh
@@ -15,8 +15,8 @@ DELAY=1
 IPERF_PORT=5001
 
 # Queue sizes to test
-QSIZES=(20 100)
-# QSIZES=(10 60)
+# QSIZES=(20 100)
+QSIZES=(10 60)
 # note: run QSIZES=(20) and QSIZES=(100) separately when running with AQM
 # QSIZES=(20)
 # QSIZES=(100)
